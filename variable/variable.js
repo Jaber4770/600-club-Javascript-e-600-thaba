ae2e29cd51f26e087a602324ae72e852833c004a
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-03-24 01:29:06 +0200

    variable done

---
+++ variable/variable.js
@@ -0,0 +1,170 @@
+const birthYear = 2003;
+const sibling = 3;
+const students = 45;
+const marks = 40;
+const rooms = 3;
+const studyTime = 6;
+const total = 42;
+
+const age = 23;
+console.log(age);
+const bookToRead = 3;
+console.log(bookToRead);
+const weakUpTime = 7;
+console.log(weakUpTime);
+const wastedTime = 3;
+console.log(wastedTime);
+const borrow = 316000;
+console.log(borrow);
+const goals = 4;
+console.log(goals);
+const pillow = 2;
+console.log(pillow);
+
+
+const favFood = 'biriyani';
+const hidingPlace = "bed er niche!";
+const drinking = "tea";
+const subject = "english";
+const teacher = "Dreagon Delwar";
+const phone = "Samsung";
+const fruit = "watermelon";
+
+
+const name = "Jaber Ahmed";
+console.log(name);
+const address = "bucharest, romania";
+console.log(address);
+const bestFriendUniveristy = "univeristy of cassino";
+console.log(bestFriendUniveristy);
+const fan = "kew nai";
+console.log(fan);
+const movieOfTheYear = "Intersteller";
+console.log(movieOfTheYear);
+const hobby = "planting tree";
+const favColor = "black";
+console.log(favColor);
+
+const isRich = true;
+const passed = false;
+console.log(isRich);
+console.log(passed);
+
+const afterEatingBumboIsHappy = false;
+console.log(afterEatingBumboIsHappy);
+const isCrushSeenMsg = false;
+console.log(isCrushSeenMsg);
+
+const celebrating = true;
+console.log(celebrating);
+
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy, hasMoney, isHungry);
+
+const isStudyDone = true;
+console.log(isStudyDone);
+
+const isPhoneCharged = true;
+console.log(isPhoneCharged);
+
+const isDinnerDone = false;
+console.log(isDinnerDone);
+
+
+// sum
+{
+    const eggPrice = 10;
+    const onionPrice = 20;
+    const total = eggPrice + onionPrice;
+    console.log(total);
+}
+
+// substract
+
+{
+    const eggPrice = 10;
+    const onionPrice = 20;
+    const result = eggPrice - onionPrice;
+    console.log(result);
+}
+
+{
+    const eggPrice = 10;
+    const onionPrice = 20;
+    const result = onionPrice - eggPrice;
+    console.log(result);
+}
+
+{
+    const eggPrice = 10;
+    const onionPrice = 20;
+    const result = onionPrice / eggPrice;
+    console.log(result);
+}
+
+// more practice
+
+{
+    const earning = 100;
+    const expense = 40;
+    const remaining = earning - expense;
+    console.log(remaining);
+} 
+
+{
+    const pencil = 10;
+    const usePencil = 2;
+    const useTime = pencil / usePencil;
+    console.log(useTime);
+}
+{
+    const moneyHave =60;
+    const everyTimeExpense = 15;
+    const howManyTimeICanBuy = moneyHave % everyTimeExpense;
+    console.log(howManyTimeICanBuy);
+}
+{
+    const hello = "Hello";
+    const world = "World";
+    const helloworld = hello + world;
+    console.log(helloworld);
+}
+{
+    const rice = 153;
+    const dal = 261;
+    const totalWeight = rice + dal;
+    console.log(totalWeight);
+}
+{
+    const moneyIHave = 500;
+    const everyTimeIBuy = 75;
+    console.log("koto tk baki thake", moneyIHave % everyTimeIBuy);
+    console.log("koto bar khoroch kora jabe. ", moneyIHave / everyTimeIBuy);
+}
+{
+    const number =8;
+    const divider = 3;
+    console.log("result: ", number / divider);
+    console.log("reminder: ", number % divider);
+}
+{
+    const number = 50;
+    const divider = 9;
+    console.log("result: ", number % divider);
+}
+{
+    const bangla = "Bangla";
+    const desh = "desh";
+    console.log(bangla + desh);
+}
+{
+    const everyDayIEatInKG = 4;
+    const totalRiceinMon = 12;
+    const monToTotalKG = totalRiceinMon * 40;
+    const howManyDayIcanEat = monToTotalKG / everyDayIEatInKG;
+    const month = 30;
+    const howManyMonth = howManyDayIcanEat / month;
+    console.log("koto din: ",howManyDayIcanEat, "koto month: ",howManyMonth);
+}
