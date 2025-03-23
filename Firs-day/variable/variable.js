4c6fd82a9db0a9ae4f9a8bff924990fa26d2c2ef
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-03-24 01:00:30 +0200

    learning variable

---
+++ Firs-day/variable/variable.js
@@ -0,0 +1,80 @@
+const birthYear = 2003;
+const sibling = 3;
+const students = 45;
+const marks = 40;
+const rooms = 3;
+const studyTime = 6;
+const total = 42;
+
+const age = 23;
+console.log(age);
+const bookToRead = 3;
+console.log(bookToRead);
+const weakUpTime = 7;
+console.log(weakUpTime);
+const wastedTime = 3;
+console.log(wastedTime);
+const borrow = 316000;
+console.log(borrow);
+const goals = 4;
+console.log(goals);
+const pillow = 2;
+console.log(pillow);
+
+
+const favFood = 'biriyani';
+const hidingPlace = "bed er niche!";
+const drinking = "tea";
+const subject = "english";
+const teacher = "Dreagon Delwar";
+const phone = "Samsung";
+const fruit = "watermelon";
+
+
+const name = "Jaber Ahmed";
+console.log(name);
+const address = "bucharest, romania";
+console.log(address);
+const bestFriendUniveristy = "univeristy of cassino";
+console.log(bestFriendUniveristy);
+const fan = "kew nai";
+console.log(fan);
+const movieOfTheYear = "Intersteller";
+console.log(movieOfTheYear);
+const hobby = "planting tree";
+const favColor = "black";
+console.log(favColor);
+
+const isRich = true;
+const passed = false;
+console.log(isRich);
+console.log(passed);
+
+const afterEatingBumboIsHappy = false;
+console.log(afterEatingBumboIsHappy);
+const isCrushSeenMsg = false;
+console.log(isCrushSeenMsg);
+
+const celebrating = true;
+console.log(celebrating);
+
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy, hasMoney, isHungry);
+
+const isStudyDone = true;
+console.log(isStudyDone);
+
+const isPhoneCharged = true;
+console.log(isPhoneCharged);
+
+const isDinnerDone = false;
+console.log(isDinnerDone);
+
+
+
+
+
+
+
