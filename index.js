96b2516f538f1177f2f5cc670bdd07b047a00a5f
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-03-23 22:59:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("git commit -m "first commit"
+git branch - M main
+git remote add origin https://github.com/Jaber4770/600-club-Javascript-e-600-thaba.git
+    git push - u origin main");
