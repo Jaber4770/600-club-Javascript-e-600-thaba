131dfcb90aae7c241fd6f3ac11b78f3f20c147af
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-03-24 18:31:05 +0200

    done

---
+++ type/type.js
@@ -0,0 +1,86 @@
+const price = 555;
+console.log(typeof (price));
+const learning = true;
+console.log(typeof learning);
+
+const megaCity = "Dhaka";
+console.log(typeof (megaCity));
+
+const tempareture = 102.5;
+console.log(typeof (tempareture));
+
+const isHungry = true;
+console.log(typeof (isHungry));
+
+const isRich = true;
+console.log(typeof isRich);
+
+
+
+let age = 23;
+console.log(age);
+age = 25;
+console.log(age);
+
+let pocketMoney = 500;
+console.log(pocketMoney);
+pocketMoney = 150;
+console.log(pocketMoney);
+
+let studyTime = 4;
+console.log(studyTime);
+studyTime = 0;
+console.log(studyTime);
+
+let friendsCount = 3;
+console.log(friendsCount);
+friendsCount = 1;
+console.log(friendsCount);
+
+let phonePrice = 25000;
+console.log(phonePrice);
+phonePrice = 12000;
+console.log(phonePrice);
+
+let chairsCount = 40;
+console.log(chairsCount);
+chairsCount = 40 - 2;
+console.log(chairsCount);
+
+let playTime = 3;
+console.log(playTime);
+playTime = 0;
+console.log(playTime);
+
+// cricket score, total run of BD
+const score = 280;
+
+// border radius 10px
+// border radius in all site
+// border rounded
+const radius = 10;
+
+/* 
+i have 3 favouriste cloths
+
+*/
+const dress = 3;
+
+/* i will study total 7 hours
+first e math
+sencond phhysic
+third FEE
+*/
+const studyTimes = 7;
+
+/* send money to purchase keyboard, and service fee 50tk */
+const sendMoney = 3000;
+
+// delivery fe 50 + product er dam 300
+const pay = 350;
+
+ 
+
+
+
+
