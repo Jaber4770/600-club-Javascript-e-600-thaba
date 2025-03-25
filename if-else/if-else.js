e8d308471f92c6beb5337ce4a963505344728fb9
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-03-25 23:30:47 +0200

    if else practice

---
+++ if-else/if-else.js
@@ -0,0 +1,192 @@
+const time = 6;
+if (time > 5) {
+    console.log("get nasta from ammo.");
+} else {
+    console.log("get jharor bari");
+}
+
+const login = true;
+if (login == true) {
+    console.log("Welcome!");
+} else {
+    console.log("Get Lost!");
+}
+
+const myRun = 5;
+if (myRun === 5) {
+    console.log("get chocolate");
+}
+else {
+    console.log("You will have mota vuri.");
+}
+
+const myMarks = 85;
+if (myMarks > 80) {
+    console.log("baba will buy a bike for me.");
+} else {
+    console.log("no bike");
+}
+
+const movieShowTime = 8;
+if (movieShowTime < 9) {
+    console.log("i wil watch the movie.");
+}
+else {
+    console.log("nakee tel diye ghomabo.");
+}
+
+const temparature = 30;
+if (temparature >= 30) {
+    console.log("Turn on the Air Conditioner.");
+} else {
+    console.log("kombol mori diye ghomabo.");
+}
+
+
+const age = 18;
+const height = 65;
+if (age > 18 && height > 60) {
+    console.log("gari thelo");
+} else {
+    console.log("garite boshe thako");
+}
+
+const math = 82;
+const english = 87;
+if (math > 80 || english > 85) {
+    console.log("you can continue study");
+}
+else {
+    console.log("toke biye diye dibo.");
+}
+
+const gpa = 5;
+const earning = 9200;
+if (gpa === 5 && earning <= 10000) {
+    console.log("you will get scholarship.");
+}
+else {
+    console.log("you will not get scholarhsip")
+}
+
+
+const candidateAge = 29;
+const experience = 3;
+if (candidateAge < 30 && experience > 2) {
+    console.log("you can seat for the exam.");
+} else {
+    console.log("sorry.");
+}
+
+const hasEgg = 13;
+const hasChicken = false;
+if (hasEgg > 12 || hasChicken === true) {
+    console.log('dimer korma hobe');
+} else {
+    console.log("pawroti and kola khabo.");
+}
+
+
+const bodyTemparature = 98;
+const hasKashi = false;
+if (bodyTemparature >= 100 && hasKashi == true) {
+    console.log("go to doctor");
+}
+else {
+    console.log("kathha mori diye suiye thako");
+}
+
+const attendance = 78;
+const submitAssignment = false;
+if (attendance > 80 && submitAssignment === true) {
+    console.log("you can seat for the exam.");
+} else {
+    console.log("you can not seat for the exam.");
+}
+
+
+const electricity = true;
+const mobileCharge = false;
+if (electricity === false && mobileCharge === false) {
+    console.log("you will study");
+}
+else {
+    console.log("you will play video game.");
+}
+
+const shirtPrice = 1200;
+const hasCoupon = true;
+if (shirtPrice > 1000 && hasCoupon === true) {
+    console.log(shirtPrice - (shirtPrice * 20 / 100));
+}
+else {
+    console.log("no discount");
+}
+
+
+// discount:
+
+const price = 4500;
+if (price >= 6000) {
+    const discount = (price / 100) * 15;
+    const pay = price - discount;
+    console.log(pay);
+}
+else if (price >= 3000) {
+    const discount = (price / 100) * 5;
+    const pay = price - discount;
+    console.log(pay);
+}
+
+
+{
+    const age = 6;
+    if (age <= 12) {
+        console.log("free food");
+    }
+    else if (age >= 60) {
+        console.log("get 50% discount");
+    }
+    else {
+        console.log("you have to pay full bill.")
+    }
+
+}
+
+
+
+const balance = 1300;
+if (balance < 1000) {
+    console.log(
+        "deposit money"
+    )
+} else if (balance > 1000 && balance < 5000) {
+    console.log("bindas life enjoy koro");
+}
+else if (balance > 5000) {
+    console.log("you are rich. marry me.");
+}
+
+
+const marks = 85;
+if (marks < 50) {
+    console.log("you fail.");
+} else if (marks > 50 && marks < 80) {
+    console.log("passed");
+} else if (marks > 80) {
+    console.log("A+")
+}
+
+const totalPage = 446;
+if (totalPage < 100) {
+    console.log("small size book");
+} else if (totalPage > 100 && totalPage < 500) {
+    console.log("mid size book");
+} else if (totalPage > 500) {
+    console.log("heart attack size book");
+}
+
+
+
+
+
