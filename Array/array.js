ae9623c294c6dc77508c1d4dcf8b524da353894a
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-03-26 01:46:24 +0200

    learning array

---
+++ Array/array.js
@@ -0,0 +1,35 @@
+const numbers = [71, 72, 73, 74, 75, 76, 77, 78, 79];
+const fruits = ["apple", "banana", "orange", "mango", "goava"];
+const shoppingBag = ["alo", "peyaj", "begon", "mola", "gajor", "tomato", "derosh", "law", "sosha", "potol"];
+const favMovie = ["titanic", "interstellar", "bahubali", "kgf", "r jani na"];
+const oddNum = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
+const saroundColor = ["black", "white", "blue", "red"];
+console.log(saroundColor.length);
+const capital = ["dhaka", "washington", "bucharest", "rome", "berlin"];
+console.log(capital.length);
+
+
+const friends = ["rifat", "shawon", "mithu", "ashik", "olik"];
+console.log(friends[3]);
+
+const favBook = ["book1", "book2", "book3", "book4", "book5", "book6", "book7"];
+console.log(favBook[5]);
+
+const favGame = ["free fire", "pubgi", "candi crash", "temple run"];
+favGame[2] = "subway surfer";
+console.log(favGame);
+
+const familyMembers = ["baba", "ma", "jisun", "ami(Jack)", "mim"];
+console.log(familyMembers[3]);
+
+
+const travelPlan = ["switzerland", "norway", "denmark", "germany", "australia", "canada", "usa"];
+console.log(travelPlan[4]);
+
+const studyTable = ["book", "monitor", "laptop", "speaker", "mouse", "keyboard"];
+console.log(studyTable[7]);
+
+
+
+
+
