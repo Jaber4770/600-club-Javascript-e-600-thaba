72f6fa1e3aa2a9464543ae4d4473723ca35730fe
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-03-26 10:06:17 +0200

    printed exam date

---
+++ Loop/Loop.js
@@ -0,0 +1,44 @@
+const subjects = ['physics', 'biology', 'higher math', 'chemistry', 'bangla'];
+for (const subject of subjects) {
+    console.log(subject);
+}
+
+const foods = ['biriyani', "kabab", "goro", "morgi", 'vera', "vegitable"];
+for (const food of foods) {
+    console.log(food);
+}
+
+const birthYears = [2001, 2002, 2003, 2004, 2005, 2006];
+for (const birthYear of birthYears) {
+    console.log(birthYear);
+}
+
+const favPlayers = ['tamim', 'shakib', 'mashrafi', 'mushfiq', 'riyad', 'miraj', "taskin"];
+for (const player of favPlayers) {
+    console.log(player);
+}
+
+const examDate = ['12 april', '13 april', '14 april'];
+for (const exam of examDate) {
+    console.log(exam);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
