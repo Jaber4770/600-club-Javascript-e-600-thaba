d0018366b73b1df745cada6834f12ff72fc386e6
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-03-27 13:59:17 +0200

    learning object

---
+++ Object/object.js
@@ -0,0 +1,92 @@
+const teacher = {
+    name: "Koddos Munshi",
+    subject: "Higher Math",
+    location: "kazipara",
+    age: 50,
+    college: "A Monem College"
+}
+console.log(teacher);
+
+
+const tree = {
+    name: 'mango tree',
+    age: 40,
+    height: "30 fit",
+    fruitName: "mango"
+}
+console.log(tree);
+
+
+const prani = {
+    name: "robel",
+    age: 32,
+    study: "masters",
+    religion: 'muslim',
+    height: "65 inch"
+}
+console.log(prani);
+
+
+const dad = {
+    name: "ramzan ali",
+    profession: "teacher",
+    age: 47,
+    child: 3
+}
+console.log(dad);
+
+const motorBike = {
+    brand: 'BMW',
+    taier: "black",
+    speed: 300,
+    price: 240000
+}
+console.log(motorBike);
+
+const bird = {
+    name: 'duel',
+    color: 'black',
+    food: 'poka-makor',
+    location: 'bd'
+}
+console.log(bird);
+
+const laptop = {
+    brand: 'dell',
+    processor: 'intel',
+    ram: '8gb',
+    price: 35000,
+    displaySize: 15
+}
+console.log(laptop);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
