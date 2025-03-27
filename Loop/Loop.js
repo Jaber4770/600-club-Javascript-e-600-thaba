9196a342e3b653d331111d8aa13ca83e4c3a187a
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-03-27 13:19:04 +0200

    object comming

--- Loop/Loop.js
+++ Loop/Loop.js
@@ -173,5 +173,51 @@ for (let i = 1; i <= 50; i++){
 console.log(sUM);
 
 
+for (let i = 1; i <= 30; i++){
+    if (i > 15) {
+        break;
+    }
+    console.log(i);
+}
+
+for (let i = 1; i <= 40; i++){
+    if (i % 7 === 0) {
+        continue;
+    }
+    console.log(i);
+}
+
+
+for (let i = 1; i <= 15; i++){
+    if (i === 9) {
+        continue;
+    }
+    console.log(i);
+}
+
+for (let i = 1; i <= 20; i++){
+    if (i === 12) {
+        continue;
+    }
+    console.log(i);
+}
+
+for (let i = 1; i <= 25; i++){
+    if (i % 3 === 0) {
+        continue;
+    }
+    console.log(i);
+}
+
+for (let i = 91; i <= 120; i++){
+    if (i % 10 === 0) {
+        break;
+    }
+    console.log("91-120: ",i);
+}
+
+
+
+
 
 
