fdce7279e0bf4272d64dbacbd3feeb114cf6894b
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-03-29 12:55:13 +0200

    object

--- Object/object.js
+++ Object/object.js
@@ -60,23 +60,80 @@ const laptop = {
 }
 console.log(laptop);
 
+//  -------------------------------
+
+const player = {
+    name: 'jack',
+    age: 23,
+    sports: 'cricket',
+    team: 'bangladesh'
+}
+console.log(player.team);
+
+const lapTop = {
+    brand: 'dell',
+    price: 30,
+    hardDisk: 500,
+    ram: 16,
+    screenSize: 15
+}
+console.log(lapTop.screenSize);
 
 
+const favPlace = {
+    name: "Cox's Bazar",
+    distance: "400km",
+    popularity: 'high'
+}
+console.log(favPlace["popularity"])
 
 
+const phone = {
+    brand: 'Nokia',
+    color: 'black',
+    price: 5000
+}
+console.log(phone["price"]);
 
 
+const library = {
+    name: 'Public Library',
+    location: "Dhaka",
+    books: 5000
+}
+console.log(library["books"]);
+
+const movies = {
+    title: 'inception',
+    director: "Nolan",
+    rating: 9
+}
+console.log(movies["rating"]);
 
 
+const college = {
+    name: 'ndc',
+    established: 1949,
+    group: ['science', 'arts', 'commerce']
+}
+console.log(college.group[1]);
 
 
-
-
-
-
-
-
-
+const family = {
+    "fathe.r": {
+        name: "baba",
+        age: 45,
+        profession: "teacher"
+    },
+    mother: {
+        name: "mom",
+        age: 35,
+        profession: 'teacher'
+    }
+}
+console.log(family.mother.age);
+console.log(family['fathe.r'].age);
+console.log("age sum: ", family.mother.age + family['fathe.r'].age);
 
 
 
