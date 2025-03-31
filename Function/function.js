f2f2e7ca6146fc20fe88ee4b619b72d36c48fbf5
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-04-01 00:59:34 +0300

    function firrst practice done

--- Function/function.js
+++ Function/function.js
@@ -2,3 +2,57 @@ function juiceKhao() {
     console.log('ei naw lebor jouce.');
 }
 juiceKhao();
+
+function myRoutine() {
+    console.log("ghom theke uthe coffee khai");
+    console.log("tar por pc open kori");
+    console.log("js er book pori");
+}
+myRoutine();
+
+function fmaily() {
+    console.log("mom")
+    console.log("dad")
+    console.log("jisun")
+    console.log("sijan")
+    console.log("mim")
+}
+fmaily();
+
+function taskDone() {
+    console.log("programming in the morning")
+    console.log("delivery")
+    console.log("cooking")
+    console.log("delivery")
+    console.log('programming')
+}
+taskDone();
+
+function greetMe() {
+    console.log("Good Morning! Have a great day!");
+}
+greetMe();
+
+function getFavFriends() {
+    console.log('rifat');
+    console.log('shawon');
+    console.log('mithu');
+}
+getFavFriends();
+
+function introductionMyself() {
+    const myself = {
+        name: "jack",
+        age: 23,
+        address: 'bucharest, ro',
+        phone: +40737778201,
+        height: 5.6,
+        favFood: 'biriyani'
+    }
+    for (let clue in myself) {
+        console.log(clue, myself[clue]);
+    }
+}
+introductionMyself();
+
+
