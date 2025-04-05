091a6910e13b5196928570a82661fca0c3356a9b
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-04-05 14:54:24 +0300

    some every flat

--- ES6/arrayMethod.js
+++ ES6/arrayMethod.js
@@ -46,3 +46,23 @@
     const animals = ['cow', 'goat', 'sheep', 'horse'];
     animals.forEach(animal => console.log(animal));
 }
+{
+    const numbers = [90, 50, 110, 102, 91, 150];
+    const larger = numbers.some(number => number > 100);
+    console.log(larger);
+}
+{
+    const elements = [10, 20, 30, 40, 50, 60];
+    const divBy5 = elements.every(element => element % 5 === 0);
+    console.log(divBy5);
+}
+{
+    const words = ['mango', 'hero', 'hello', "banana", 'hijibiji'];
+    const findHello = words.some(word => word === "hello");
+    console.log(findHello);
+}
+{
+    const ages = [18, 19, 20, 31, 21, 22, 23, 24, 24, 25, 26];
+    const adult = ages.every(age => age >= 18);
+    console.log(adult);
+}
